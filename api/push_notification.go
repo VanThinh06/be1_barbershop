4722e4eefa56de91331cbf5e5f40045c3119b7d9
VanThinh06 <dvanthinh06@gmail.com>
2023-07-16 10:00:26 +0700

    abc

--- api/push_notification.go
+++ api/push_notification.go
@@ -8,13 +8,13 @@ import (
 
 	"google.golang.org/api/option"
 )
-  func sdkFirebaseAdmin()  {
+
+func sdkFirebaseAdmin() {
 	opt := option.WithCredentialsFile("./barbershop-75055-firebase-adminsdk-dvogj-080ebc30c9.json")
 	config := &firebase.Config{ProjectID: "barbershop-75055"}
 	app, err := firebase.NewApp(context.Background(), config, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
-	
-	
-  }
+
+}
