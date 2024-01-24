1aea06f0874a99f2bb922f53f5252be6c8688f54
DuongVanThinh <dvanthinh06@gmail.com>
2024-01-24 17:38:18 +0700

    tabel barber

--- src/db/query/barber_managers.sql
+++ src/db/query/barber_managers.sql
@@ -14,13 +14,6 @@ SELECT *
 FROM "BarberManagers"
 WHERE "id" = $1;
 
--- name: UpdateBarberManagers :one
-UPDATE "BarberManagers"
-SET "manager_id" = $1,
-    "update_at" = NOW()
-WHERE "id" = $2
-RETURNING *;
-
 -- name: DeleteBarberManagers :exec
 DELETE FROM "BarberManagers"
 WHERE "id" = $1;
